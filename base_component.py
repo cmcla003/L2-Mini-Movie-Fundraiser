30cb702e867cf8aa4dc5e3d3372435742b766fac
Cindy  McLaren <51941943+cmcla003@users.noreply.github.com>
2023-03-23 21:56:14 +0000

    base component not working at this stage work in progress

--- base_component.py
+++ base_component.py
@@ -1,27 +1,22 @@
 # Function goes here
-def yes_no(question):
-    valid = False
-    while not valid:
-        # Ask user if they have played before
-        response = input(question).lower().strip()
+# sting check indexes lists to asses input and tailor error message 
+def string_check(question, num_letter, valid_list):
 
-        # If  yes, output 'program continues'
-        if response == "yes" or response == "y":
-            response = "yes"
-            return response
+  error = "Please choose {} or {} ".format(valid_list[0],valid_list[1])
+  if num_letter == 1:
+    version = 1
+  else: 
+    version = 2
+  
+  while True:
+    # Ask user if they have played before
+    response = input(question).lower().strip()
+    for item in valid_list: 
+          if response == item[:version] or response == item:
+            return item
+    print(error)
 
-        # If no output 'display instructions'
-        elif response == "no" or response == "n":
-            response= "no"
-            return response
-
-        else:
-            print("Please enter yes or no")
-
-# start of loop
-
-# intialise variables
-
+# assesses input is not blank 
 def not_blank(question):
     valid = False
     while not valid:
@@ -36,13 +31,18 @@ def not_blank(question):
 # initalise variables 
 
 # initalise variables
+
+# Lists and initialise variable 
+yes_no = ["yes","no"]
+payment = ["cash","credit"]
+
 name = ""
 count = 0
 MAX_TICKETS = 5
 
 # *** Main routine ***
 # ask user if want instructions 
-want_instructions = yes_no("Do you want to read the instructions? ")
+instructions = string_check("Please enter yes or no: ",1, yes_no)
 if want_instructions == "yes":
   # print detailed instructions at a later date
   print("Instructions go here")
@@ -52,13 +52,24 @@ while name != "xxx" and count < MAX_TICKETS:
     # tells user how many tickets left
     if count < 4:
         print("You have {} places left".format(MAX_TICKETS-count))
-    #  on place left
+    # one place left
     else:
         print("***You have ONE place left***")
 
     # get details
     name = not_blank("Name: ")
     count +=1
+    age = int(input("Age: "))
 
+    # calculate ticket price based on age
+    if age < 16:
+        ticket_price = 7.5
+    
+    elif age < 65:
+        ticket_price = 10.5
+    
+    else:
+        ticket_price = 6.5
+  
 if count == MAX_TICKETS:
     print("You have sold all available tickets.")
