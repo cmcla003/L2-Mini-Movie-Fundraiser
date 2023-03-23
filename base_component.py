8a37c2787ae7e9f764d54bd682b28f7402533415
Cindy  McLaren <51941943+cmcla003@users.noreply.github.com>
2023-03-23 22:36:55 +0000

    takes in name and age to calculate ticket price based on input

--- base_component.py
+++ base_component.py
@@ -8,7 +8,7 @@ def not_blank(question):
             return response
         else:
             print("This can't be blank please enter a name")
-def number_check (question,low_num,high_num):
+def number_check(question,low_num,high_num):
     error = "Please enter a whole number between {} and {} ".format(low_num, high_num)
 
     valid = False
@@ -25,7 +25,14 @@ def number_check (question,low_num,high_num):
         except ValueError:
             print(error)
 
-
+def calc_ticket_price(var_age):
+   if age < 16:
+     price = 7.5
+   elif age < 65:
+     price = 10.5
+   else:
+     price = 6.5
+   return price
 def string_check(question, num_letter, valid_list):
   error = "Please choose {} or {} ".format(valid_list[0],valid_list[1])
   if num_letter == 1:
@@ -44,51 +51,38 @@ def string_check(question, num_letter, valid_list):
 def instructions ():
   print("Instructions go here.... to be completed ")
 
-# main routine goes here
-# initalise variables 
-
-# initalise variables
 
 # Lists and initialise variable 
-
 yes_no = ["yes","no"]
 payment = ["cash","credit"]
-
 name = ""
 count = 0
 MAX_TICKETS = 5
 
 # *** Main routine ***
 # ask user if want instructions 
-want_instructions = string_check("Please enter yes or no: ",1, yes_no)
+want_instructions = string_check("Do you want instructions enter yes or no: ",1, yes_no)
 if want_instructions == "yes":
   # print detailed instructions at a later date
   instructions()
   
-
-
 while name != "xxx" and count < MAX_TICKETS:
-    # tells user how many tickets left
-    if count < 4:
-        print("You have {} places left".format(MAX_TICKETS-count))
-    # one place left
-    else:
-        print("***You have ONE place left***")
+  # tells user how many tickets left
+  if count < 4:
+      print("You have {} places left".format(MAX_TICKETS-count))
+  # one place left
+  else:
+      print("***You have ONE place left***")
 
-    # get details
-    name = not_blank("Name: ")
-    count +=1
-    age = number_check(input("Age: ",12, 120))
+  # get user details check age and calculate ticket price
+  name = not_blank("Name: ")
+  count +=1
+  age = number_check("Age: ",12, 120)
+  ticket_cost= calc_ticket_price(age)
 
-    # calculate ticket price based on age
-    if age < 16:
-        ticket_price = 7.5
-    
-    elif age < 65:
-        ticket_price = 10.5
-    
-    else:
-        ticket_price = 6.5
+  # print for testing 
+  print("Name: {} Age: {} Ticket price ${}".format(name,age,ticket_cost))
   
-if count == MAX_TICKETS:
+  #if ALL ticekts sold
+  if count == MAX_TICKETS:
     print("You have sold all available tickets.")
