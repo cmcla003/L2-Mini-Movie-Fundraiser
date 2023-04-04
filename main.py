75fbb66b985cc83a3f3638035e7cef6389ebead7
Cindy  McLaren <51941943+cmcla003@users.noreply.github.com>
2023-04-04 23:53:45 +0000

    String checker  removed num_letter iteration and fed input directly into the condition

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-import base_component
+import string_checker
--- string_checker.py
+++ string_checker.py
@@ -2,16 +2,12 @@
 def string_check(question, num_letter, valid_list):
 
   error = "Please choose {} or {} ".format(valid_list[0],valid_list[1])
-  if num_letter == 1:
-    version = 1
-  else: 
-    version = 2
   
   while True:
     # Ask user if they have played before
     response = input(question).lower().strip()
     for item in valid_list: 
-          if response == item[:version] or response == item:
+          if response == item[:num_letter] or response == item:
             return item
     print(error)
 
